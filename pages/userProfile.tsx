73ebc6db571c509588258e9fb8109c5297b2ff80
Mae830 <55216013+Mae830@users.noreply.github.com>
2020-11-09 15:15:37 -0500

    Made the edit button work

--- pages/userProfile.tsx
+++ pages/userProfile.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Head from 'next/head';
 import AppMenu from '../components/AppMenu';
-import { Paper, Box, TextField, Typography, Grid, makeStyles } from '@material-ui/core';
+import { Paper, Box, TextField, Typography, Grid, makeStyles, Dialog, DialogTitle, DialogActions, DialogContent, DialogContentText, Button } from '@material-ui/core';
 import styles from './userProfile.module.scss';
 import { Avatar, Fab } from '@material-ui/core';
 import EditIcon from '@material-ui/icons/Edit';
@@ -15,6 +15,36 @@ const useStyle = makeStyles(theme => ({
 }));
 
 export default function profile() {
+  let [open, setOpen] = React.useState(false);
+  let [firstName, setFirstName] = React.useState('');
+  let [lastName, setLastName] = React.useState('');
+  let [model, setModel] = React.useState('');
+  let [make, setMake] = React.useState('');
+  let [plate, setPlate] = React.useState('');
+
+  let [firstNameTemp, setFirstNameTemp] = React.useState('');
+  let [lastNameTemp, setLastNameTemp] = React.useState('');
+  let [modelTemp, setModelTemp] = React.useState('');
+  let [makeTemp, setMakeTemp] = React.useState('');
+  let [plateTemp, setPlateTemp] = React.useState('');
+
+  const changeProfile =
+  (firstNameTemp: string, lastNameTemp: string, modelTemp: string, makeTemp: string, plateTemp: string) => {
+    setFirstName(firstNameTemp);
+    setLastName(lastNameTemp);
+    setModel(modelTemp);
+    setMake(makeTemp);
+    setPlate(plateTemp);
+    setOpen(false);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setFirstName('');
+    setLastName('');
+    setModel('');
+    setMake('');
+  };
   const classes = useStyle();
   return <React.Fragment>
     <Head>
@@ -26,15 +56,32 @@ export default function profile() {
         <Avatar className={classes.large}/>
       </Grid>
       <Grid>
-        <Typography variant="h6" align="left">Annie Caroline</Typography>
-        <Typography variant="h6" align="left">Car: </Typography>
-        <Typography variant="h6" align="left">Make: Ford </Typography>
-        <Typography variant="h6" align="left">Model: Escort</Typography>
-        <Typography variant="h6" align="left">Plate ID:  FDT357</Typography>
+        <Typography variant="h6" align="left">User Name: {firstName} {lastName}</Typography>
+        <Typography variant="h6" align="left">Car Information</Typography>
+        <Typography variant="h6" align="left">Make: {make} </Typography>
+        <Typography variant="h6" align="left">Model: {model}</Typography>
+        <Typography variant="h6" align="left">Plate ID: {plate}</Typography>
       </Grid>
     </Paper>
-    <Fab className={styles.fab} color="secondary" aria-label="edit">
+    <Fab className={styles.fab} onClick={() => setOpen(true)} color="secondary" aria-label="edit">
       <EditIcon />
     </Fab>
+    <Dialog open={open} onClose={() => setOpen(false)}>
+      <DialogTitle> Create Lot </DialogTitle>
+      <DialogContent>
+        <DialogContentText> Edit  This form to edit your Profile</DialogContentText>
+        <TextField value={firstNameTemp} label="First Name" variant="outlined" margin="normal" fullWidth required onChange={event => setFirstNameTemp(event.target.value)}>{firstName}</TextField>
+        <TextField value={lastNameTemp} label="Last Name" variant="outlined" margin="normal" fullWidth required onChange={event => setLastNameTemp(event.target.value)}>{lastName}</TextField>
+        <TextField value={makeTemp} label="Make" variant="outlined" margin="normal" fullWidth required onChange={event => setMakeTemp(event.target.value)}>{make}</TextField>
+        <TextField value={modelTemp} label="Model" variant="outlined" margin="normal" fullWidth required onChange={event => setModelTemp(event.target.value)}>{model}</TextField>
+        <TextField value={plateTemp} label="Model" variant="outlined" margin="normal" fullWidth required onChange={event => setPlateTemp(event.target.value)}>{plate}</TextField>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => handleClose()}> Cancel </Button>
+        <Button onClick={() => changeProfile(firstNameTemp, lastNameTemp, makeTemp, modelTemp, plateTemp)}>
+        Edit
+        </Button>
+      </DialogActions>
+    </Dialog>
   </React.Fragment>;
 }
