58e5b0a9e72ca58edcd323a25a597b90300e02dd
Nicholas Salazar <nsalazar1@ufl.edu>
2020-12-07 23:20:23 -0500

    Added Vehicle API (Essentially, get all vehicles of the current user from vehicles table) lmk if u want more endpoints bby kthnxbai

---
+++ server/api/vehicles.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import db from '../db';
+import { wrapAsync } from '../common';
+import { validateSession } from './accounts';
+
+let router = express.Router(); // eslint-disable-line new-cap
+let jsonParse = bodyParser.json();
+let cookieParse = cookieParser();
+
+router.use(jsonParse);
+router.use(cookieParse);
+
+router.get('/', (_req, res) => res.send({ status: 'ok' }));
+
+router.get('/user/self', validateSession, wrapAsync(async (req, res) => {
+    if (!req.session) {
+        res.status(404).send({
+          status: 'error',
+          error: 'NO_SESSION',
+          description: 'session does not exist'
+        });
+        return;
+      }
+      let vehicles = (await db.query('SELECT plateid FROM vehicles WHERE userid = $1', [req.session.userId])).rows[0];
+      res.status(200).send({
+        status: 'ok',
+        vehicles: vehicles.rows
+      });
+  }));
+
+  export default router;
