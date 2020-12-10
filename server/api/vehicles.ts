4ebfd529959b2cc2b4601a61136b6a711b6e1094
Nicholas Salazar <nsalazar1@ufl.edu>
2020-12-09 19:16:01 -0500

    Added remove vehicle

--- server/api/vehicles.ts
+++ server/api/vehicles.ts
@@ -15,19 +15,36 @@ router.use(cookieParse);
 router.get('/', (_req, res) => res.send({ status: 'ok' }));
 
 router.get('/user/self', validateSession, wrapAsync(async (req, res) => {
-    if (!req.session) {
-        res.status(404).send({
-          status: 'error',
-          error: 'NO_SESSION',
-          description: 'session does not exist'
-        });
-        return;
-      }
-      let vehicles = (await db.query('SELECT plateid FROM vehicles WHERE userid = $1', [req.session.userId]));
-      res.status(200).send({
-        status: 'ok',
-        vehicles: vehicles.rows
-      });
-  }));
+  if (!req.session) {
+    res.status(404).send({
+      status: 'error',
+      error: 'NO_SESSION',
+      description: 'session does not exist'
+    });
+    return;
+  }
+  let vehicles = (await db.query('SELECT plateid FROM vehicles WHERE userid = $1', [req.session.userId]));
+  res.status(200).send({
+    status: 'ok',
+    vehicles: vehicles.rows
+  });
+}));
 
-  export default router;
+/*
+DELETE Request
+*/
+router.delete('/:plateId', wrapAsync(async (req, res) => {
+  let plateId = req.params.plateId;
+  let query = await db.query('DELETE CASCADE FROM vehicles WHERE plateId = $1', [plateId]);
+  if (!query.rowCount) {
+    res.status(404).send({
+      status: 'error',
+      error: 'NONEXISTENT_VEHICLE',
+      description: 'plate does not exist'
+    });
+  } else {
+    res.status(200).send({ status: 'ok' });
+  }
+}));
+
+export default router;
